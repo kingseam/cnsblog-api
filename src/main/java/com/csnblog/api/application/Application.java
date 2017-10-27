6031cc210c6875d115ab75fc0664b925aa3e57f8
164114 <164114@shinsegae.com>
2017-10-27 13:40:08 +0900

    + mysql setting add

--- src/main/java/com/csnblog/api/application/Application.java
+++ src/main/java/com/csnblog/api/application/Application.java
@@ -1,12 +1,12 @@
-package com.csnblog.api.cnsblog_api;
+package com.csnblog.api.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CnsblogApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CnsblogApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
