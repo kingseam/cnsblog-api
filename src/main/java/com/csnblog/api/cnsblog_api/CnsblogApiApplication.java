d6a7d119c3a36f63a1722061c5a182e28dada10a
164114 <164114@shinsegae.com>
2017-10-27 11:08:05 +0900

    + spring boot init

---
+++ src/main/java/com/csnblog/api/cnsblog_api/CnsblogApiApplication.java
@@ -0,0 +1,12 @@
+package com.csnblog.api.cnsblog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsblogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsblogApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/csnblog/api/cnsblog_api/CnsblogApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.csnblog.api.cnsblog_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CnsblogApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
