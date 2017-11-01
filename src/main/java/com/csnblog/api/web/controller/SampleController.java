8fb750a09227f639e8e32d3879549323b1920d29
164114 <164114@shinsegae.com>
2017-11-01 13:23:17 +0900

    + User Table schema update

--- src/main/java/com/csnblog/api/web/controller/SampleController.java
+++ src/main/java/com/csnblog/api/web/controller/SampleController.java
@@ -1,6 +1,5 @@
 package com.csnblog.api.web.controller;
 
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.csnblog.api.common.constant.ResponseAttribute;
@@ -22,56 +22,55 @@ import com.google.common.collect.Maps;
 @RequestMapping("/sample")
 public class SampleController {
 
-    private final SampleService sampleService;
+	private final SampleService sampleService;
 
-    @Autowired
-    public SampleController(SampleService sampleService) {
-        this.sampleService = sampleService;
-    }
+	@Autowired
+	public SampleController(SampleService sampleService) {
+		this.sampleService = sampleService;
+	}
 
+	@GetMapping("/list")
+	public String sampleController() throws IOException {
+		Map responseResult = new HashMap();
+		JsonResponseTemplate jsonResponseTemplate = new JsonResponseTemplate(responseResult) {
+			@Override
+			public void doJob() throws IOException {
 
-    @GetMapping("/list")
-    public String sampleController() throws IOException {
-        Map responseResult = new HashMap();
-        JsonResponseTemplate jsonResponseTemplate = new JsonResponseTemplate(responseResult) {
-            @Override
-            public void doJob() throws IOException {
+				Sample sampleResultData = sampleService.getSampleList();
 
-                Sample sampleResultData = sampleService.getSampleList();
+				responseResult.put(ResponseAttribute.RESULTS, sampleResultData);
+			}
+		};
+		return jsonResponseTemplate.getResultArrayJson();
+	}
 
-                responseResult.put(ResponseAttribute.RESULTS, sampleResultData);
-            }
-        };
-        return jsonResponseTemplate.getResultArrayJson();
-    }
+	@GetMapping("/list2")
+	public @ResponseBody ResponseDto sample2Controller() {
+		Map<String, Object> result = Maps.newHashMap();
 
-    @GetMapping("/list2")
-    public ResponseDto sample2Controller(){
-    	Map<String, Object> result = Maps.newHashMap();
+		if ("로그인".equals("로긴")) {
+			return new ResponseDto.Builder().useResFail(ResponseCode.FAIL).build();
+		}
 
-    	if("로그인".equals("로긴")){
-    		return new ResponseDto.Builder().useResFail(ResponseCode.FAIL).build();
-    	}
+		Sample sampleResultData = sampleService.getSampleList();
+		result.put("sampleResultData", sampleResultData);
 
-    	Sample sampleResultData = sampleService.getSampleList();
-    	result.put("sampleResultData", sampleResultData);
+		return new ResponseDto.Builder(result).build();
 
-    	return new ResponseDto.Builder(result).build();
+	}
 
-    }
+	@GetMapping("/list3")
+	public @ResponseBody ResponseDto sample3Controller() {
+		ResponseDto resDto = new ResponseDto.Builder().build();
 
-    @GetMapping("/list3")
-    public ResponseDto sample3Controller(){
-    	ResponseDto resDto = new ResponseDto.Builder().build();
+		if ("로그인".equals("로긴")) {
+			return new ResponseDto.Builder().useResFail(ResponseCode.FAIL).build();
+		}
 
-    	if("로그인".equals("로긴")){
-    		return new ResponseDto.Builder().useResFail(ResponseCode.FAIL).build();
-    	}
+		Sample sampleResultData = sampleService.getSampleList();
+		resDto.getData().put("sampleResultData", sampleResultData);
 
-    	Sample sampleResultData = sampleService.getSampleList();
-    	resDto.getData().put("sampleResultData", sampleResultData);
+		return resDto;
 
-    	return resDto;
-
-    }
+	}
 }
