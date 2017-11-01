ea47bda40ecff4e08f1e478f10d37ef026f8bfdf
164114 <164114@shinsegae.com>
2017-11-01 17:24:32 +0900

    + jwt resource setting add - properties => yml - key add

--- src/main/java/com/csnblog/api/web/controller/MainController.java
+++ src/main/java/com/csnblog/api/web/controller/MainController.java
@@ -1,13 +1,28 @@
 package com.csnblog.api.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    @RequestMapping(value = "/")
+	@GetMapping(value = "/")
     public String app() {
+        return "CNS BLOG :: API";
+    }
+
+	@GetMapping(value = "/simple/test")
+    public String test() {
+        return "CNS BLOG :: API22";
+    }
+
+	@GetMapping(value = "/anonymous")
+    public String anonymous() {
+        return "CNS BLOG :: API";
+    }
+
+	@GetMapping(value = "/api/test")
+    public String anonymous2() {
         return "CNS BLOG :: API";
     }
 }
