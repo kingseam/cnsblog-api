b760ab26c716e888cf0a2620d618498c2ad9584a
164114 <164114@shinsegae.com>
2017-11-01 12:07:56 +0900

    + list2 , list3 commit, 3 case need selector

--- src/main/java/com/csnblog/api/web/controller/SampleController.java
+++ src/main/java/com/csnblog/api/web/controller/SampleController.java
@@ -1,18 +1,22 @@
 package com.csnblog.api.web.controller;
 
 
-import com.csnblog.api.common.constant.ResponseAttribute;
-import com.csnblog.api.web.controller.template.JsonResponseTemplate;
-import com.csnblog.api.web.domain.Sample;
-import com.csnblog.api.web.service.SampleService;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import com.csnblog.api.common.constant.ResponseAttribute;
+import com.csnblog.api.common.dto.ResponseDto;
+import com.csnblog.api.common.enumclass.ResponseCode;
+import com.csnblog.api.web.controller.template.JsonResponseTemplate;
+import com.csnblog.api.web.domain.Sample;
+import com.csnblog.api.web.service.SampleService;
+import com.google.common.collect.Maps;
 
 @RestController
 @RequestMapping("/sample")
@@ -39,5 +43,35 @@ public class SampleController {
             }
         };
         return jsonResponseTemplate.getResultArrayJson();
+    }
+
+    @GetMapping("/list2")
+    public ResponseDto sample2Controller(){
+    	Map<String, Object> result = Maps.newHashMap();
+
+    	if("로그인".equals("로긴")){
+    		return new ResponseDto.Builder().useResFail(ResponseCode.FAIL).build();
+    	}
+
+    	Sample sampleResultData = sampleService.getSampleList();
+    	result.put("sampleResultData", sampleResultData);
+
+    	return new ResponseDto.Builder(result).build();
+
+    }
+
+    @GetMapping("/list3")
+    public ResponseDto sample3Controller(){
+    	ResponseDto resDto = new ResponseDto.Builder().build();
+
+    	if("로그인".equals("로긴")){
+    		return new ResponseDto.Builder().useResFail(ResponseCode.FAIL).build();
+    	}
+
+    	Sample sampleResultData = sampleService.getSampleList();
+    	resDto.getData().put("sampleResultData", sampleResultData);
+
+    	return resDto;
+
     }
 }
