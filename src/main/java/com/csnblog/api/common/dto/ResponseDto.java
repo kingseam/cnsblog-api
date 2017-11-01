05312b146972391a189c27414463e976777a3e5b
164114 <164114@shinsegae.com>
2017-11-01 11:33:12 +0900

    + code convention fix

--- src/main/java/com/csnblog/api/common/dto/ResponseDto.java
+++ src/main/java/com/csnblog/api/common/dto/ResponseDto.java
@@ -8,54 +8,54 @@ import lombok.Getter;
 
 @Getter
 public class ResponseDto {
-    private ResponseCode code;
-    private Map<String, Object> data;
+	private ResponseCode code;
+	private Map<String, Object> data;
 
-    private ResponseDto(Builder builder) {
+	private ResponseDto(Builder builder) {
 		this.code = builder.code;
 		this.data = builder.data;
 	}
 
-    public static class Builder {
-        private ResponseCode code;
-        private Map<String, Object> data;
+	public static class Builder {
+		private ResponseCode code;
+		private Map<String, Object> data;
 
-        public Builder() {
-        	code = ResponseCode.DEFAULT;
-        	data = null;
+		public Builder() {
+			code = ResponseCode.DEFAULT;
+			data = null;
 		}
 
-        public Builder(ResponseCode code) {
-        	this.code = code;
+		public Builder(ResponseCode code) {
+			this.code = code;
 		}
 
-        public Builder(Map<String, Object> data) {
-        	this.data = data;
+		public Builder(Map<String, Object> data) {
+			this.data = data;
 		}
 
-        public Builder(ResponseCode code, Map<String, Object> data) {
-        	this.code = code;
-        	this.data = data;
+		public Builder(ResponseCode code, Map<String, Object> data) {
+			this.code = code;
+			this.data = data;
 		}
 
-        public Builder useResFail() {
-        	this.code = ResponseCode.FAIL;
-        	return this;
-        }
+		public Builder useResFail() {
+			this.code = ResponseCode.FAIL;
+			return this;
+		}
 
-        public Builder useResFail(ResponseCode code) {
-        	this.code = code;
-	        return this;
-	    }
+		public Builder useResFail(ResponseCode code) {
+			this.code = code;
+			return this;
+		}
 
-        public Builder useResData(Map<String, Object> data){
-        	this.data = data;
-        	return this;
-        }
+		public Builder useResData(Map<String, Object> data) {
+			this.data = data;
+			return this;
+		}
 
-        public ResponseDto build() {
-	        return new ResponseDto(this);
-	    }
+		public ResponseDto build() {
+			return new ResponseDto(this);
+		}
 
-    }
+	}
 }
