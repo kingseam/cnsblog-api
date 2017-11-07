c059a1fd25a99f65cb335af374c2961c2e6e175f
164114 <164114@shinsegae.com>
2017-11-07 13:26:31 +0900

    - sampleController junit fix;

--- src/test/java/com/csnblog/api/web/controller/SampleControllerTest.java
+++ src/test/java/com/csnblog/api/web/controller/SampleControllerTest.java
@@ -1,26 +1,31 @@
 package com.csnblog.api.web.controller;
 
-import com.csnblog.api.web.mapper.SampleMapper;
-import com.csnblog.api.web.service.SampleService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.csnblog.api.web.mapper.SampleMapper;
+import com.csnblog.api.web.service.SampleService;
+
 @RunWith(SpringRunner.class)
-@WebMvcTest(SampleController.class)
+@SpringBootTest
+@WebAppConfiguration
 public class SampleControllerTest {
-    @Autowired
     private MockMvc mockMvc;
 
     @MockBean
@@ -28,6 +33,14 @@ public class SampleControllerTest {
 
     @MockBean
     private SampleMapper sampleMapper;
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
 
     @Test
     public void sampleRequestController_with_parameter() throws Exception {
