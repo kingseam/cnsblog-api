95b47ede48a8845dfe5f7c97f7c179f1df8842ef
ljh3259 <ljh3259@naver.com>
2017-11-15 17:45:08 +0900

    add spring aop @Around

--- src/main/java/com/csnblog/api/common/aop/SampleAspect.java
+++ src/main/java/com/csnblog/api/common/aop/SampleAspect.java
@@ -1,8 +1,10 @@
 package com.csnblog.api.common.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -18,6 +20,36 @@ public class SampleAspect {
         Signature signature = joinPoint.getSignature();
 
         log.info("call method before :: {}", signature.getName());
+    }
+
+
+    @Around("within(com.csnblog.api.web.controller.*)")
+    public Object logBefore(ProceedingJoinPoint point) throws Throwable {
+
+        long start = System.currentTimeMillis();
+        Object resultVal = point.proceed();
+        long processTime = System.currentTimeMillis() - start;
+
+        StringBuilder sb = new StringBuilder();
+        Object[] params = point.getArgs();
+
+        int i=0;
+        for(Object param : params) {
+            if(param != null) {
+                if(i > 0)	sb.append(", ");
+                sb.append(param.toString());
+                i++;
+            }
+        }
+
+        log.info("111");
+        log.info("---------------------------------------------------------------------------------------------------------------------------");
+        log.info("Processing Time("+ point.getSignature().toShortString() +") : " + processTime + " ms");
+        log.info("Param : " + sb.toString());
+        log.info("Result : " + resultVal.toString());
+        log.info("---------------------------------------------------------------------------------------------------------------------------");
+
+        return resultVal;
     }
 
     @After("execution(* com.csnblog.api..*.*(..))")
