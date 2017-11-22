ce0cf612c40c5f6854206592dd59a34a51fc364f
ChangYong <lcy9002@naver.com>
2017-11-22 20:39:35 +0900

    remove componentScan annotation

--- src/main/java/com/csnblog/api/Application.java
+++ src/main/java/com/csnblog/api/Application.java
@@ -3,10 +3,8 @@ package com.csnblog.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
-@ComponentScan({"com.csnblog.api"})
 @MapperScan("com.csnblog.api.web.mapper")
 public class Application {
 
