1e2361e900e4d88856813f68633452b126c9bccb
164114 <164114@shinsegae.com>
2017-11-27 17:36:47 +0900

    - sample controller remove;

--- src/test/java/com/cnsblog/api/web/controller/SampleControllerTest.java
+++ src/test/java/com/cnsblog/api/web/controller/SampleControllerTest.java
@@ -1,17 +1,10 @@
 package com.cnsblog.api.web.controller;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,22 +27,11 @@ public class SampleControllerTest {
 
     @Test
     public void sampleRequestController_with_parameter() throws Exception {
-        this.mockMvc.perform(post("/sample/request").contentType(MediaType.APPLICATION_JSON)
-                .content("{\"param\":{\"name\":\"name\",\"age\":29}}"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("{\"check\":true}")))
-                .andExpect(content().string(containsString("success")));
+
     }
 
     @Test
     public void sampleRequestController_with_parameter_collection() throws Exception {
-        this.mockMvc.perform(post("/sample/request/collection").contentType(MediaType.APPLICATION_JSON)
-                .content("{\"param\":{\"persons\":[{\"name\":\"name\",\"age\":29}]}}"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("\"outerCheck\":true")))
-                .andExpect(content().string(containsString("\"innerCheck\":true")))
-                .andExpect(content().string(containsString("success")));
+
     }
 }
