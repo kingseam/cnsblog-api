5621868a33e662091c3dbdbf5cf765cde43b47a0
164114 <164114@shinsegae.com>
2018-01-18 09:22:26 +0900

    - CrossDomain config add;

--- src/main/java/com/cnsblog/api/common/ws/WebSocketConfig.java
+++ src/main/java/com/cnsblog/api/common/ws/WebSocketConfig.java
@@ -2,10 +2,12 @@ package com.cnsblog.api.common.ws;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
+@CrossOrigin
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
